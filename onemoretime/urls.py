8600914103965f98de612a262a8bcfc6734b8cd3
Joy <jj0985788718@gmail.com>
2024-08-14 18:54:04 +0800

    index set

--- onemoretime/urls.py
+++ onemoretime/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-
+from core.views import index
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
 urlpatterns+=staticfiles_urlpatterns()
