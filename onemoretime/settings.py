38892686d4be35367102692d0c75f56129a3a509
Joy <jj0985788718@gmail.com>
2024-08-17 19:02:24 +0800

    logo fixed

--- onemoretime/settings.py
+++ onemoretime/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
