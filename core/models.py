77bf5fc2dc47063f96afc612421ee85657dbef89
Joy <jj0985788718@gmail.com>
2024-08-19 21:12:42 +0800

    carousel pic

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Carousel(models.Model):
+    name = models.TextField(max_length=10, blank=True)
+    image = models.ImageField(upload_to='item_images')
+
+    def __str__(self):
+        return self.name
