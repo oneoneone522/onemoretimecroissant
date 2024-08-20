b1054d6f784149218e05b55b9ee04d660774fd14
Joy <jj0985788718@gmail.com>
2024-08-20 18:51:41 +0800

    init

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Carousel,News
+from .models import Carousel,News, Storeimg
 
 admin.site.register(Carousel)
 admin.site.register(News)
+admin.site.register(Storeimg)
--- core/models.py
+++ core/models.py
@@ -12,3 +12,6 @@ class News(models.Model):
     image = models.ImageField(upload_to='core_images')
     def __str__(self):
         return self.name
+class Storeimg(models.Model):
+    image = models.ImageField(upload_to='core_images')
+    the_big = models.BooleanField(default=False)
