9b8f191c068cb9328e41425373e6783039479273
Joy <jj0985788718@gmail.com>
2024-08-21 13:49:32 +0800

    media root changed

--- onemoretime/settings.py
+++ onemoretime/settings.py
@@ -126,13 +126,16 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
 
 # STATIC_ROOT = "vol/static"
 # MEDIA_ROOT = "vol/media"
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
