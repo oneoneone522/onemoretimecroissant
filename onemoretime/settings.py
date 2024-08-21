915a304d9fad7fe6c0576099b8ff8c317e58599f
Joy <jj0985788718@gmail.com>
2024-08-21 17:06:38 +0800

    debug-true

--- onemoretime/settings.py
+++ onemoretime/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
@@ -133,10 +133,10 @@ STATICFILES_DIRS = [
 
 # STATIC_ROOT = "vol/static"
 # MEDIA_ROOT = "vol/media"
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
