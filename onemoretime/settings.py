6302fcaa32275e509a49bf7230ad1bc414270d35
Joy <jj0985788718@gmail.com>
2024-08-22 19:04:50 +0800

    production env set up

--- onemoretime/settings.py
+++ onemoretime/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
@@ -125,16 +125,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
+
+
+STATIC_ROOT = "vol/static"
+MEDIA_ROOT = "/vol/media/"
+
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 STATICFILES_DIRS = [
     BASE_DIR/"static",
 ]
-
-# STATIC_ROOT = "vol/static"
-# MEDIA_ROOT = "vol/media"
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # MEDIA_URL = os.getenv('MEDIA_URL', '/media/')
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
---
+++ proxy/default.conf.tpl
@@ -0,0 +1,15 @@
+server {
+    listen ${LISTEN_PORT};
+
+    location /static {
+        alias /vol/static/;
+    }
+    location /media {
+        alias /vol/media/;
+    }
+
+    location / {
+        proxy_pass http://${APP_HOST}:${APP_PORT};
+        include    /etc/nginx/proxy_params;
+    }
+}
