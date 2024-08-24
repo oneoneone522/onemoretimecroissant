4c1dd45803b6521c4f430570d5227fde9f7ffe48
Joy <jj0985788718@gmail.com>
2024-08-24 23:44:59 +0800

    news ticker

--- core/views.py
+++ core/views.py
@@ -3,10 +3,14 @@ from item.models import Item
 from .models import Carousel, News
 def index(request):
     items = Item.objects.filter(hot_item = True)[0:3]
+    context = {
+        'repeat_count': range(20)  # 這裡定義要重複的次數
+    }
     carousels = Carousel.objects.all()
     return render(request,'core/index.html',{
         'items':items,
         'carousels':carousels,
+        'context':context,
     })
 def news(request):
     news = News.objects.all()
