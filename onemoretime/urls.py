50d362bfecd5e3a14a514265962bf7f744954e67
Joy <jj0985788718@gmail.com>
2024-08-27 16:54:14 +0800

    aws connection fixed

--- onemoretime/urls.py
+++ onemoretime/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('', include('core.urls')),
     path('item/', include('item.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
