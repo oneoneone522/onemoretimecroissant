5298cb5c7d2f8b8be419d7bc7e9c82af1eb2bdab
oneoneone522 <165547050+oneoneone522@users.noreply.github.com>
2024-12-01 17:14:53 -0800

    Update models.py
    set order

--- item/models.py
+++ item/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    order = models.PositiveIntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
+        ordering = ['order']
 
     def __str__(self):
         return self.name
