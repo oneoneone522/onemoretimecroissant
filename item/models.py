21ffe0754777d0ee148cb7f1a7c20bc9adf915bb
oneoneone522 <165547050+oneoneone522@users.noreply.github.com>
2024-12-01 17:31:57 -0800

    Update models.py
    Commenting out order function

--- item/models.py
+++ item/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
-    order = models.PositiveIntegerField(default=0)
+    #order = models.PositiveIntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
-        ordering = ['order']
+        #ordering = ['order']
 
     def __str__(self):
         return self.name
