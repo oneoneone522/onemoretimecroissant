100ada8e063a928f5b47854133016f639ad46155
oneoneone522 <jj0985788718@gmail.com>
2024-12-09 12:29:26 -0800

    field added

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from .models import Carousel, Notification, News, Storeimg
 def index(request):
     items = Item.objects.filter(hot_item = True)[0:3]
     context = {
-        'repeat_count': range(20)  # 這裡定義要重複的次數
+        'repeat_count': range(20)
     }
     carousels = Carousel.objects.all()
     notices = Notification.objects.all()
--- item/admin.py
+++ item/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Category, Item, Box
 
-admin.site.register(Category)
 admin.site.register(Item)
 admin.site.register(Box)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'order')  # Display the 'name' and 'order' fields in the admin list view
+    list_editable = ('order',)       # Allow the 'order' field to be editable directly in the admin list view
+    ordering = ('order',) 
